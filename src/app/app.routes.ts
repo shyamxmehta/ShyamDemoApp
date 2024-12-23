bfd68da010fdb5c51f6a0a49423a8a66478342ca
shyam <shyamxmehta@gmail.com>
2024-12-23 15:36:42 +0300

    FINISHED SIDEBAR COMPONENT

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { authGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { InventoryComponent } from './inventory/inventory.component';
+import { EndOfDayComponent } from './end-of-day/end-of-day.component';
 
 export const routes: Routes = [
     
     { path: '', component: HomeComponent, title: 'Home', canActivate: [authGuard], children: 
         [
-            { path: 'inventory', component: InventoryComponent, title: 'Inventory'}
+            { path: 'inventory', component: InventoryComponent, title: 'Inventory'},
+            { path: 'endofday', component: EndOfDayComponent, title: 'End of Day'}
         ]
     },
 
---
+++ src/app/end-of-day/end-of-day.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end-of-day',
+  standalone: true,
+  imports: [],
+  templateUrl: './end-of-day.component.html',
+  styleUrl: './end-of-day.component.scss'
+})
+export class EndOfDayComponent {
+
+}
