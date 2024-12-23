394ff61fc8e0878dda3a753af4f4681b9a572199
shyamxmehta <shyamxmehta@gmail.com>
2024-12-23 09:12:24 +0300

    STILL WORKING ON LOGIN DESIGN

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './auth/auth.guard';
+import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    { path: '', component: HomeComponent, canActivate: [authGuard] },
+
+    { path: 'login', component: LoginComponent, title: 'Login' }
+];
