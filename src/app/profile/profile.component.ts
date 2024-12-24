9e074155a5724aaee75703603513004a39d2d457
shyam <shyamxmehta@gmail.com>
2024-12-24 13:03:19 +0300

    WORKING ON PROFILE PAGE

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+
+  eodRights: boolean = true;
 
 }
