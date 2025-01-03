4760462a6944a8bf617af0ea9251c2e2f5ced728
shyam <shyamxmehta@gmail.com>
2025-01-03 13:00:07 +0300

    WORKING ON PAGINATION

--- src/app/end-of-day/end-of-day.component.ts
+++ src/app/end-of-day/end-of-day.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { BreadcrumbsComponent } from "../breadcrumbs/breadcrumbs.component";
 import { TableComponent } from "./table/table.component";
 import { FormsModule } from '@angular/forms';
+import { PaginationComponent } from "../shared/pagination/pagination.component";
 
 @Component({
   selector: 'app-end-of-day',
   standalone: true,
-  imports: [BreadcrumbsComponent, TableComponent, FormsModule],
+  imports: [BreadcrumbsComponent, TableComponent, FormsModule, PaginationComponent],
   templateUrl: './end-of-day.component.html',
   styleUrl: './end-of-day.component.scss'
 })
