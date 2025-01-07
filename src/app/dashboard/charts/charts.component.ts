33a768a645ef3faab784cd75520751a976ca0495
shyam <shyamxmehta@gmail.com>
2025-01-07 10:53:04 +0300

    FIXED CHARTS

--- src/app/dashboard/charts/charts.component.ts
+++ src/app/dashboard/charts/charts.component.ts
@@ -16,40 +16,50 @@ export class ChartsComponent implements OnInit {
   itemsService = inject(ItemsService);
   products: Product[] = [];
   labelData: any = [];
-  realData: any = [];
+  valueData: any = [];
 
 
   ngOnInit(): void {
     this.itemsService.getProducts.subscribe(products => {
+      //get complete dataset
       this.products = products;
+      //check for actual data
       if (this.products.length > 0) {
-        this.products.map(o => {
-          const label = o.ProductDescription?.split(' ', 2);
-          console.log(label);
+        //sort data in order desired eg asc/desc
+        this.products.sort((a: Product, b: Product) => {
+          if (a.CostPrice! > b.CostPrice!) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+        //get sorted data
+        const top10Products: Product[] = this.products.slice(0, 10);
+        //get label and value data
+        top10Products.forEach(v => {
+          const label = v.ProductDescription?.split(' ', 2);
           this.labelData.push(label);
-          this.realData.push(o.CostPrice);
-        })
-        this.realData.sort((a: any,b: any) => {
-          if (a > b) {
-            return -1
-          } else {return 1}
-        })
-        this.renderBarChart(this.labelData, this.realData);
-      }
+          this.valueData.push(v.CostPrice);
+        });
+
+        this.renderBarChart(this.labelData, this.valueData);
+        
+      }        
+
     });
-    const [a] = this.labelData;
-    console.log(a);
+    
+
     this.renderPieChart();
   }
 
-  renderBarChart(labelData: any, realData: any) {
+  renderBarChart(labelData: [], valueData: number) {
     const mychar = new Chart( 'bar-chart', {
       type: 'bar',
       data: {
         labels: labelData,
         datasets: [
           {
-            data: realData,
+            data: valueData,
             backgroundColor: '#2c4e80',
             barThickness: 25,
             borderRadius: 3,
@@ -120,6 +130,13 @@ export class ChartsComponent implements OnInit {
     };
 
     const options: ChartOptions = {
+      //remove pie chart border
+      elements: {
+        arc: {
+          borderWidth: 0
+        }
+      },
+      //
       responsive: false,
       maintainAspectRatio: false,
       aspectRatio: 1.5,
@@ -127,6 +144,7 @@ export class ChartsComponent implements OnInit {
         legend: {
           position: 'right'
         },
+        //moidify labels
         datalabels: {
           formatter: function(value) {
             return value + '%'
